5ee37b3f62a0f6754d42318c6ad49c32a7a51ac6
Alberto Basile <vandalo72@gmail.com>
2014-07-08 14:48:32 +0100

    Owin and WebApi
    Demonstrate the use of WebApi with Owin.

---
+++ OwinWebApi/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(OwinWebApi.Startup))]
+
+namespace OwinWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            //  Enable attribute based routing
+            //  http://www.asp.net/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+
+            app.UseWelcomePage("/");
+            app.UseErrorPage();
+        }
+    }
+}
---
+++ OwinWebApi/api/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OwinWebApi.api
+{
+    [RoutePrefix("api/1.0.0")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("test")]
+        public async Task<IHttpActionResult> Get()
+        {
+            string name = "Mister";
+            string sayHello = string.Empty;
+
+            Task<string> t = new Task<string>(() =>
+            {
+                sayHello = string.Format("Hello {0}", name);
+                return sayHello;
+            });
+            t.Start();
+            await t;
+
+            return Ok(new string[] { sayHello });
+        }
+    }
+}
