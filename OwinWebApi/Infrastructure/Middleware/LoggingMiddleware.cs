1b60690277b03f6d43318bf9b343f97e65ce858b
Alberto Basile <vandalo72@gmail.com>
2014-10-24 17:28:16 +0100

    Added custom Middleware
    Added custom Middleware

---
+++ OwinWebApi/Infrastructure/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OwinWebApi.Infrastructure.Middleware
+{
+    public class LoggingMiddleware : OwinMiddleware
+    {
+        public LoggingMiddleware(OwinMiddleware next)
+            : base(next)
+        {
+        }
+
+        public async override Task Invoke(IOwinContext context)
+        {
+            Console.WriteLine("Begin Request");
+            await Next.Invoke(context);
+            Console.WriteLine("End Request");
+        }
+    }
+}
